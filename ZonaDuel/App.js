b5026a4aa36292323076a3a20e0ac6685dace02a
Djamel BENDAOUD <Djamel@MacBook-Pro-de-Djamel.local>
2017-03-27 15:46:36 +0200

    izi

--- ZonaDuel/App.js
+++ ZonaDuel/App.js
@@ -1,30 +1,64 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
   AppRegistry,
+  StyleSheet,
   Text,
   Button,
-  View
+  View,
+  Image
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 
-
 class HomeScreen extends React.Component {
   static navigationOptions = {
-    title: 'Welcome',
+    title: 'ZonaDuel',
   };
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <View>
-        <Text>Hello, Navigation!</Text>
+    <View style={styles.container}>
+        <View style={styles.more}>
+            <Text style={{textAlign: 'center', fontFamily: 'Gill Sans',}}>
+            ZonaDuel, un jeu rapide, amusant te permettant de défier tes amis professionnels de santé sur le Zona.
+            Qui sera le meilleur ?
+           </Text>
+          </View>
+          <View style={styles.quiz}>
         <Button
           onPress={() => navigate('Quizz')}
-          title="Quizz"/>
+          title="Let's go!"/>
+          </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  title: {
+
+  },
+  more: {
+    borderRadius: 10,
+    marginTop: 60,
+    backgroundColor: 'skyblue',
+  },
+  quiz: {
+    backgroundColor: 'orange',
+    borderWidth: 2,
+    borderRadius: 5,
+    margin: 120,
+    marginTop: 400,
+
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#3399ff',
+  },
+
+
+})
+
 
 class Quizz extends React.Component {
   static navigationOptions = {
